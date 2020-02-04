ceb32b43493bc239b6fa2b34a3d201d2d6b97bc6
dmseo1 <dkfk2747@gmail.com>
2020-02-04 17:50:53 +0900

    init project

--- mypicker/ViewController.swift
+++ mypicker/ViewController.swift
@@ -8,12 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    
+    @IBOutlet weak var showPicker: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        showPicker.tintColor = .clear
+        createPickerView()
+        dismissPickerView()
         // Do any additional setup after loading the view.
     }
+    
+    let fruits = ["사과", "배", "포도", "망고", "딸기", "바나나", "파인애플"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruits.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+       return fruits[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        showPicker.text = fruits[row]
+    }
+    
+    
+    func createPickerView() {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        showPicker.inputView = pickerView
+    }
+    
+    func dismissPickerView() {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let button = UIBarButtonItem(title: "선택", style: .plain, target: self, action: #selector(self.action))
+        toolBar.setItems([button], animated: true)
+        toolBar.isUserInteractionEnabled = true
+        showPicker.inputAccessoryView = toolBar
+    }
+    
+   @objc func action() {
+         
+      }
+
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
 
 }
